4598342c6c0b2fb7ca244b014f5405f15a756efd
zaynab18 <zaynabemadi@gmail.com>
2023-11-03 09:48:24 +0330

    add nodes and elements matrices plus adding boundary conditions

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ import numpy as np
 
 # nodal matrix statically
 # it should be a matrix with size of NoN(numbers of nodes) * PD(problem dimension)
-NodesElement = np.array([[0, 0],
-                         [1, 0],
-                         [0.5, 1]])
+nodesMatrix = np.array([[0, 0],
+                        [1, 0],
+                        [0.5, 1]])
 # matrix of elements
-# it should be a matrix with size of NoE(number of elements) * PD(problem dimension)
+# it should be a matrix with size of NoE(number of elements) * NPE(nodes per element)
+elementsMatrix = np.array([[1, 2],
+                           [2, 3],
+                           [3, 1]])
+# add boundary condition
+boundaryCondition = np.array([-1, -1],
+                             [1, -1],
+                             [1, 1])
