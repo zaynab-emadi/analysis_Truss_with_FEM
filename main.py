c2848597c4c3903744ffcb16e4e53744f4fb22e8
zaynab18 <zaynabemadi@gmail.com>
2023-11-03 09:29:48 +0330

    initialized project

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+# nodal matrix statically
+# it should be a matrix with size of NoN(numbers of nodes) * PD(problem dimension)
+NodesElement = np.array([[0, 0],
+                         [1, 0],
+                         [0.5, 1]])
+# matrix of elements
+# it should be a matrix with size of NoE(number of elements) * PD(problem dimension)
